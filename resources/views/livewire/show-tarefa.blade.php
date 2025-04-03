8ba58477d5d9314316157a73ca50c915d459cf65
AllanEwerton <allanewerton24102004@gmail.com>
2025-04-03 14:19:30 -0300

    adiciona componente Livewire para exibir tarefas e configura rotas

---
+++ resources/views/livewire/show-tarefa.blade.php
@@ -0,0 +1,7 @@
+{{-- filepath: d:\Projetos\Laravel\livewer\resources\views\livewire\show-tarefa.blade.php --}}
+<div>
+    ola mundo!
+    <p>{{ $tarefa }}</p>
+
+    <input type="text" name="tarefa" id="tarefa" wire:model="tarefa">
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Livewire\ShowTarefa;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tarefas', ShowTarefa::class)->name('tarefas.index');
