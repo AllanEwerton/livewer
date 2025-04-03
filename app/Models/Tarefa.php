f6a37699d0aae84fa19466771f97c492e3e250c8
AllanEwerton <allanewerton24102004@gmail.com>
2025-04-03 12:49:04 -0300

    adiciona estrutura inicial do projeto Laravel com configuração de arquivos e diretórios

---
+++ app/Models/Tarefa.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarefa extends Model
+{
+    protected $fillable = [
+        'titulo',
+        'descricao',
+        'prazo_final',
+        'status',
+        'usuario_id'
+    ];
+}
